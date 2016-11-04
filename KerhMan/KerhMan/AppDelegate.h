e825baf63caa86cc479f94af8f5e358f6b5c1fb5
Frederik Riedel <f@polarapps.de>
2016-11-04 21:19:54 +0100

    Added Xcode project for KehrMan

---
+++ KerhMan/KerhMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KerhMan
+//
+//  Created by Frederik Riedel on 04/11/2016.
+//  Copyright © 2016 Frogg GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KerhMan/KerhMan/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  KerhMan
+//
+//  Created by Frederik Riedel on 04/11/2016.
+//  Copyright © 2016 Frogg GmbH. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
