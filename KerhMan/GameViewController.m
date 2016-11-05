6dc9608021b435dea205b7c07a81622560b09c68
Leo Mehlig <leo@leomehlig.com>
2016-11-05 19:42:40 +0100

    Speed and steering

--- KerhMan/GameViewController.m
+++ KerhMan/GameViewController.m
@@ -9,6 +9,8 @@
 #import "GameViewController.h"
 #import "GameScene.h"
 #import "MapScene.h"
+#import "AMGSoundManager.h"
+#import "AMGAudioPlayer.h"
 
 @implementation GameViewController
 
@@ -42,6 +44,18 @@
     
     self.sceneKitView.scene = mapScene;
     [mapScene moveCamera];
+    
+//    AMGSoundManager* soundManager = [AMGSoundManager sharedManager];
+//    [soundManager playAudio:[[NSBundle mainBundle] pathForResource:@"driving" ofType:@"mp3"] withName:@"driving" inLine:@"driving" withVolume:1 andRepeatCount:-1 fadeDuration:0.5 withCompletitionHandler:nil];
+//    
+//    [NSTimer scheduledTimerWithTimeInterval:7 repeats:YES block:^(NSTimer * _Nonnull timer) {
+//        [soundManager setVolume:0.3 forLine:@"driving" withFadeDuration:1];
+//        [soundManager playAudio: [[NSBundle mainBundle] pathForResource:@"drifting" ofType:@"mp3"] withName:@"drifting" inLine:@"drifting" withVolume:0.8 andRepeatCount:0 fadeDuration:0.2 withCompletitionHandler:^(BOOL success, BOOL stopped) {
+//        }];
+//        [soundManager setVolume:1 forLine:@"driving" withFadeDuration:5];
+//
+//    }];
+    
     
     //self.sceneKitView.allowsCameraControl = YES;
     
